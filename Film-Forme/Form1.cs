975744d78795dcd0dd6d538bfb51006e2c5fe895
IgorJanjic003 <ogiigorjanjic@gmail.com>
2021-12-14 01:18:19 +0100

    Radi valjda

--- Film-Forme/Form1.cs
+++ Film-Forme/Form1.cs
@@ -13,17 +13,32 @@ namespace Film_Forme
 {
     public partial class Form1 : Form
     {
-        string cs = "Data source= ubaci si datasource; Initial catalog=Film; Integrated security=true";
+        string cs = "Data source= Unesite String; Initial catalog=film; Integrated security=true";
         DataTable podaci = new DataTable();
         int trenutni = 0;
         private void osvezi(int tren)
         {
-            id.Text = podaci.Rows[tren]["id"].ToString();
-            naziv.Text = podaci.Rows[tren]["naziv"].ToString();
-            trajanje.Text = podaci.Rows[tren]["trajanje"].ToString();
-            zanr.Text = podaci.Rows[tren]["zanr"].ToString();
-            original.Text = podaci.Rows[tren]["originalniNaziv"].ToString();
-            poreklo.Text = podaci.Rows[tren]["zemljaPorekla"].ToString();
+            if (tren < 0)
+            {
+                MessageBox.Show("Ne postoji vise instanci u bazi");
+                tren = 0;
+                id.Text = "";
+                naziv.Text = "";
+                trajanje.Text = "";
+                zanr.Text = "";
+                original.Text = "";
+                poreklo.Text = "";
+            }
+            else
+            {
+                id.Text = podaci.Rows[tren]["id"].ToString();
+                naziv.Text = podaci.Rows[tren]["naziv"].ToString();
+                trajanje.Text = podaci.Rows[tren]["trajanje"].ToString();
+                zanr.Text = podaci.Rows[tren]["zanr"].ToString();
+                original.Text = podaci.Rows[tren]["originalniNaziv"].ToString();
+                poreklo.Text = podaci.Rows[tren]["zemljaPorekla"].ToString();
+            }
+
         }
         private void uvezi()
         {
@@ -44,11 +59,12 @@ namespace Film_Forme
         private void unesi_Click(object sender, EventArgs e)
         {
             SqlConnection veza = new SqlConnection(cs);
-            string funkcija = $"INSERT INTO podaciFilm values({naziv.Text}, {trajanje.Text}, {zanr.Text}, {original.Text}, {poreklo.Text},{id.Text})";
-            SqlCommand menjanje = new SqlCommand(funkcija, veza);
+            string funkcija = $"INSERT INTO podaciFilm values({id.Text},'{naziv.Text}', '{trajanje.Text}', '{zanr.Text}', '{original.Text}', '{poreklo.Text}')";
+            SqlCommand unesi = new SqlCommand(funkcija, veza);
             veza.Open();
-            menjanje.ExecuteNonQuery();
+            unesi.ExecuteNonQuery();
             veza.Close();
+            podaci.Clear();
             uvezi();
             MessageBox.Show("Uneto!");
         }
@@ -56,6 +72,7 @@ namespace Film_Forme
         private void Form1_Load(object sender, EventArgs e)
         {
             uvezi();
+            osvezi(trenutni);
         }
 
         private void pocetak_Click(object sender, EventArgs e)
@@ -72,14 +89,13 @@ namespace Film_Forme
 
         private void sledeci_Click(object sender, EventArgs e)
         {
-            if (trenutni < podaci.Rows.Count)
+            if (trenutni < podaci.Rows.Count - 1)
             {
                 trenutni++;
-             }
+            }
             else
             {
                 trenutni = 0;
-                MessageBox.Show("Listanje je kruzno, sledbenik poslednjeg elementa je prvi");
             }
             osvezi(trenutni);
 
@@ -94,36 +110,42 @@ namespace Film_Forme
             else
             {
                 trenutni = podaci.Rows.Count - 1;
-                MessageBox.Show("Listanje je kruzno, prethodnik prvog elementa je poslednji");
+
             }
             osvezi(trenutni);
         }
 
         private void brisi_Click(object sender, EventArgs e)
         {
-                SqlConnection veza = new SqlConnection(cs);
-                SqlCommand brisanje = new SqlCommand("DELETE FROM podaciFilm WHERE id=" + id.Text, veza);
-                veza.Open();
-                brisanje.ExecuteNonQuery();
-                veza.Close();
-                uvezi();
-                podaci.Clear();
-                if (trenutni >= podaci.Rows.Count)             
-                    trenutni = podaci.Rows.Count - 1;   
-                else osvezi(trenutni);
-                MessageBox.Show("Obrisano!");
+            SqlConnection veza = new SqlConnection(cs);
+            SqlCommand brisanje = new SqlCommand("DELETE FROM podaciFilm WHERE id= " + id.Text, veza);
+            veza.Open();
+            brisanje.ExecuteNonQuery();
+            veza.Close();
+            podaci.Clear();
+            uvezi();
+            if (trenutni >= podaci.Rows.Count)
+                trenutni = podaci.Rows.Count - 1;
+            osvezi(trenutni);
+            MessageBox.Show("Obrisano!");
         }
 
         private void menjaj_Click(object sender, EventArgs e)
         {
             SqlConnection veza = new SqlConnection(cs);
-            string funkcija = $"UPDATE podaciFilm SET naziv={naziv.Text}, trajanje={trajanje.Text}, zanr={zanr.Text}, nazivOriginala={original.Text}, zemljaPorekla={poreklo.Text} WHERE id={id.Text}";
+            string funkcija = $"UPDATE podaciFilm SET naziv='{naziv.Text}', trajanje='{trajanje.Text}', zanr='{zanr.Text}', originalniNaziv='{original.Text}', zemljaPorekla='{poreklo.Text}' WHERE id={id.Text}";
             SqlCommand menjanje = new SqlCommand(funkcija, veza);
             veza.Open();
             menjanje.ExecuteNonQuery();
             veza.Close();
+            podaci.Clear();
             uvezi();
             MessageBox.Show("Promenjeno!");
+        }
+
+        private void id_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
